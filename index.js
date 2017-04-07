ca9fde62489535a75da8a00ed9d854f2d88bf0ea
Jake Rowsell <jake@qubit.com>
2017-04-08 00:39:01 +0100

    basic tests work

---
+++ index.js
@@ -0,0 +1,5 @@
+const isPrime = require('./lib/is-prime')
+
+module.exports = {
+  isPrime
+}
---
+++ test/prime-cache.test.js
@@ -0,0 +1,9 @@
+const primeCache = require('../lib/prime-cache')
+
+test('Prime cache is an object', () => {
+  expect(typeof primeCache).toEqual('object')
+})
+// get largest prime
+// check if prime
+// set prime
+// set not prime
