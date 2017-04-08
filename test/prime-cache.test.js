5120c892b9b1aed0ffd17c6c226e936c8dc0709a
Jake Rowsell <jake@qubit.com>
2017-04-08 13:52:16 +0100

    Prime cache works for getting and setting

--- test/prime-cache.test.js
+++ test/prime-cache.test.js
@@ -1,13 +1,10 @@
 const primeCache = require('../lib/prime-cache')
-// const isPrime = require('../lib/calculate-if-prime')
+const prime = 1011
+const nonPrime = 1012
 
 test('Prime cache is an object', () => {
   expect(typeof primeCache).toEqual('object')
 })
-
-// check if prime
-// set prime
-// set not prime
 
 test('Prime cache allows to get a prime number', () => {
   expect(typeof primeCache.get).toEqual('function')
@@ -17,16 +14,16 @@ test('Prime cache allows to get a prime number', () => {
   expect(typeof primeCache.set).toEqual('function')
 })
 
-test('Setting a prime and getting a prime should return true', () => {
-  expect(primeCache.get(1011)).toEqual(false)
-// primeCache.set(1011)
-// expect.
+test('Getting a prime that was not set should return false', () => {
+  expect(primeCache.get(prime)).toEqual(false)
 })
-// test('getLargestPrime returns a prime number', () => {
-//   const largestPrime = primeCache.getLargestPrime()
-//   expect(isPrime(largestPrime)).toEqual(true)
-// })
 
-// test('Prime number 2 returns true', () => {
-//   expect(primeCache.get(2)).toEqual(true)
-// })
+test('Setting a prime and getting a prime should return true', () => {
+  primeCache.set(prime, true)
+  expect(primeCache.get(prime)).toEqual(true)
+})
+
+test('Setting a non-prime and getting that number should return false', () => {
+  primeCache.set(nonPrime, false)
+  expect(primeCache.get(nonPrime)).toEqual(false)
+})
